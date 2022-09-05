4a5ab27fae39a68fa89402a1f4be75b523e1ea82
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2022-09-05 09:18:30 +0100

    revert back to vuecli

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import AppLayout from "./layouts/AppLayout.vue";
 import { createApp } from "vue/dist/vue.esm-bundler.js";
 import App from "./App.vue";
 import { createRouter, createWebHistory } from "vue-router";
-import {routes} from "./router/routes";
+import routes from "./router/routes";
 import { createMetaManager } from "vue-meta";
 
 // You can disable this if you dont want TailwindCss
@@ -11,36 +11,36 @@ import "./assets/app.css";
 import { store, key } from "./store";
 
 const router = Promise.all(routes).then((routes) => {
-  const router = createRouter({
-    history: createWebHistory(),
-    routes,
-  });
+	const router = createRouter({
+		history: createWebHistory(),
+		routes,
+	});
 
-  router.beforeEach((to, from, next) => {
-    if (!to.meta.middlewares) {
-      return next();
-    }
-    const middlewares: any = to.meta.middlewares;
-    Object.keys(middlewares).forEach((middleware) => {
-      middlewares[middleware]({ to, from, next });
-    });
-    return next();
-  });
+	router.beforeEach((to, from, next) => {
+		if (!to.meta.middlewares) {
+			return next();
+		}
+		const middlewares: any = to.meta.middlewares;
+		Object.keys(middlewares).forEach((middleware) => {
+			middlewares[middleware]({ to, from, next });
+		});
+		return next();
+	});
 
-  return router;
+	return router;
 });
 
 const init = async () => {
-  createApp({
-    components: {
-      App,
-    },
-  })
-    .component("AppLayout", AppLayout)
-    .use(await router)
-    .use(store, key)
-    .use(createMetaManager())
-    .mount("#app");
+	createApp({
+		components: {
+			App,
+		},
+	})
+		.component("AppLayout", AppLayout)
+		.use(await router)
+		.use(store, key)
+		.use(createMetaManager())
+		.mount("#app");
 };
 
 init();
